4fe77f92c7872500558920d20318350da9da6b16
macbury <macbury@gmail.com>
2012-10-22 16:16:40 +0200

    new feed engine

--- core/feed.js
+++ core/feed.js
@@ -1,5 +1,7 @@
 var FeedParser = require('feedparser');
-var request    = require('request')
+var request    = require('request');
+var FeedMe = require('feedme')
+  , parser = new FeedMe();
 var Item       = require('./item').Item;
 require('date-utils');
 
@@ -8,17 +10,45 @@ var RedisConstants = require("./constants").RedisConstants;
 var RefreshTime = 5;
 
 function Feed(obj, redisClient) {
+  this.dbObject = obj;
+  console.log("New feed parser for: "+this.dbObject.id)
   this.broken = false;
   this.redisClient = redisClient;
-  this.dbObject = obj;
-  this.parser = new FeedParser({
-    strict: true,
-  });
+  
+  this.parser = new FeedMe();
   var _this   = this;
-  /*this.parser.on('article', function(article){
-    _this.onArticle(article);
-  });*/
 
+  this.parser.on('title', function(title) {
+    console.log('title of feed is', title);
+    _this.dbObject.title = title;
+  });
+
+  this.parser.on("type", function(type) {
+    console.log("Feed type: "+ type);
+  });
+
+  this.parser.on("item", function(item) {
+    console.log("Item new");
+    //_this.onArticle(item);
+  });
+
+  this.parser.on("end", function() {
+    _this.onEnd();
+  });
+
+  try {
+    request(obj.url, { followAllRedirects: true, timeout: 10000 }).on("error", function(error){
+      _this.dbObject.errorMessage = JSON.stringify(error);
+      _this.broken = true;
+      _this.onEnd();
+    }).pipe(this.parser)
+  } catch(error) {
+    this.dbObject.errorMessage = JSON.stringify(error);
+    this.broken = true;
+    this.onEnd();
+  }
+  
+/*
   try {
     /*this.parser.parseUrl(obj.url, { normalize: true, strict: true }, function (error, meta, articles) {
       console.log("Recived callback for parse url");
@@ -29,7 +59,7 @@ function Feed(obj, redisClient) {
       }
       
       _this.onEnd();
-    });*/
+    });
 
     request(obj.url, function (error, response, body){
       if (error == null || (body == null || body.length < 10)) {
@@ -38,21 +68,18 @@ function Feed(obj, redisClient) {
             if (parseError == null) {
               _this.dbObject.title = meta.title;
               for (var i = 0; i < articles.length; i++) {
-                _this.onArticle(articles[i]);
+                //_this.onArticle(articles[i]);
               }
             } else {
               _this.dbObject.errorMessage = "Parse error";
               _this.broken = true;
             }
-
-            _this.onEnd();
           });
         } catch (parseError) {
           _this.dbObject.errorMessage = "Parse error";
           _this.broken = true;
-          _this.onEnd();
         }
-        
+        _this.onEnd();
       } else {
         _this.dbObject.errorMessage = JSON.stringify(error);
         _this.broken = true;
@@ -69,9 +96,14 @@ function Feed(obj, redisClient) {
     console.log("Timeout!");
     _this.dbObject.errorMessage = "Timeout";
     _this.broken = true; 
-    _this.parser.stream.end();
+    try {
+      _this.parser.stream.end();  
+    } catch(exception) {
+
+    }
+    
     _this.onEnd();
-  }, 15000);
+  }, 15000);*/
 }
 
 Feed.prototype.onArticle = function(article) {
@@ -91,8 +123,8 @@ Feed.prototype.onEnd = function() {
   this.dbObject.nextPull = new Date();
   this.dbObject.nextPull.addMinutes(RefreshTime * (this.dbObject.errorCount + 1));
   this.dbObject.save();
-  console.log("Finished, removing lock from feed, next check will be on: "+ JSON.stringify(this.dbObject.nextPull));
-  this.redisClient.lrem(RedisConstants.FeedLock, 0, parseInt(this.dbObject.id));
+  console.log("Finished, removing lock from feed, next check will be on: "+ JSON.stringify(this.dbObject.nextPull) + " for feed" +this.dbObject.id);
+  this.redisClient.lrem(RedisConstants.FeedLock, 0, this.dbObject.id.toString());
 }
 
 exports.Feed = Feed;
--- core/service.js
+++ core/service.js
@@ -1,7 +1,10 @@
 var Feed        = require('./feed').Feed;
-var redisClient = require("redis").createClient();
+var redis       = require("redis");
+var redisClient = redis.createClient();
 var dbHelper    = null;
 var RedisConstants = require("./constants").RedisConstants;
+
+redis.debug_mode = false;
 
 function getFeedsToSync() {
   console.log("Fetching feeds to sync");
@@ -18,13 +21,8 @@ function getFeedsToSync() {
       dbHelper.Feed.findAll({ where: [" Feeds.id NOT IN (?) AND (Feeds.nextPull IS NULL OR Feeds.nextPull < NOW()) ", a], limit: 10, order: "nextPull ASC" }).success(function(feeds) {
         for (var i = 0; i < feeds.length; i++) {
           var feedModel = feeds[i];
-          redisClient.lpush(RedisConstants.FeedLock, parseInt(feedModel.id), function(error, resp) {
-            if (error == null) {
-              var feedParser = new Feed(feedModel, redisClient);  
-            } else {
-              console.log(error);
-            }
-          });
+          redisClient.lpush(RedisConstants.FeedLock, feedModel.id.toString());
+          new Feed(feedModel, redisClient); 
         };
       });
     }
