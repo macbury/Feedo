05dd8a4ee3fd84818749acc7a343eba36b9fd658
macbury <macbury@gmail.com>
2012-10-25 15:59:03 +0200

    request api class

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,28 @@
 var asyncxml  = require('asyncxml');
 var streamify = require('dt-stream');
+
+function FeedSyncResponseBuilder(req, res) {
+  this.xml  = new asyncxml.Builder({pretty:true});
+  this.db   = req.app.get('dbHelper');
+  this.res  = res;
+  this.req  = req;
+
+  this.prepareResponse();
+  this.buildXML();
+}
+
+FeedSyncResponseBuilder.prototype.prepareResponse = function() {
+  this.xmlStream = streamify(this.xml).stream;
+  this.xmlStream.pipe(this.res);
+  this.res.write('<?xml version="1.0" encoding="UTF-8"?>\n');
+}
+
+FeedSyncResponseBuilder.prototype.buildXML = function() {
+  this.root = this.xml.tag("feedo", { version:"0.1" });
+  this.root.up().end();
+}
+
 /*
-exports.index = function(req, res){
-  res.contentType("text/xml");
-  var xml = new asyncxml.Builder({pretty:true})
-  streamify(xml).stream.pipe(res);
-  var date = new Date();
-  var db = req.app.get('dbHelper');
-  var root = xml.tag("xml", {version:"1.0"});
-
-  db.Feed.findAll({ where: { errorCount: 0 } }).success(function(feeds) {
-    var channels = root.tag("channels");
-
-    for (var i = 0; i < feeds.length; i++) {
-      var feed = feeds[i];
-      var channel = channels.tag("channel");
-        channel.tag("title", feed.title).up();
-        channel.tag("url", feed.url).up();
-        channel.tag("lastRefresh", feed.lastRefresh.toString()).up();
-      channel.up();
-    };
-
-    channels.up();
-    root.up().end();
-  }).error(function(){
-    root.up().end();
-  });
-
-  /*
-    .tag("channels")
-      .tag("channel")
-        .tag("title", "My blog posts").up()
-        .tag("link", "http://blog.local").up()
-        .tag("lastRefresh", date.toString()).up()
-      .up()
-    .up()
-  .up();
-
-
-  
-};*/
-
-exports.index = function(req, res){
-  var xml = new asyncxml.Builder({pretty:true})
-  var date = new Date();
-  var db = req.app.get('dbHelper');
-
-  res.contentType("text/xml");
-  var xmlStream = streamify(xml).stream;
-  res.write('<?xml version="1.0" encoding="UTF-8"?>\n');
-  xmlStream.pipe(res);
-  var root = xml.tag("api", {version:"1.0"});
 
   db.Item.findAll().success(function(feeds) {
     var channels = root.tag("articles");
@@ -88,4 +60,6 @@ exports.index = function(req, res){
   }).error(function(){
     root.up().end();
   });
-}
+*/
+
+exports.index = function(req, res){ new FeedSyncResponseBuilder(req, res); }
